429e1dbf9c1a76f3aff4452ee35859f6dd8e8c1e
flow <flow@hrmm.xyz>
2024-11-14 19:58:43 +0900

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<string[]>(['장보기', '강연하기']);
+  const [input, setInput] = useState('');
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Todo list</h1>
+      <input value={input} onChange={e => setInput(e.currentTarget.value)} />
+      <button
+        onClick={() => {
+          setTodos(todos => [...todos, input]);
+          setInput('');
+        }}
+      >
+        추가!
+      </button>
+      {todos.map((todo, i) => {
+        return (
+          <p
+            onClick={() => {
+              const next = [...todos];
+              next.splice(i, 1);
+              setTodos(next);
+            }}
+          >
+            {todo} {i}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
